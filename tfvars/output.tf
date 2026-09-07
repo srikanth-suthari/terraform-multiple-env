c4116145f88a7be8aa32e0122fa9f49268a690ef
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-07 20:32:25 +0530

    terraform

--- tfvars/output.tf
+++ tfvars/output.tf
@@ -1,3 +1,8 @@
 # output "remote-instance-ip" {
 #     value = aws_instance.ec2_instance.public_ip
 # }
+
+output "instance_public_ips_splat" {
+  description = "List of public IP addresses using splat syntax"
+  value       = aws_instance.web[*].public_ip
+}
