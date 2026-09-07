ab06c597a17ba4ead99b95004b52473dc2ddd3cd
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-07 20:32:48 +0530

    terraform

--- tfvars/output.tf
+++ tfvars/output.tf
@@ -4,5 +4,5 @@
 
 output "instance_public_ips_splat" {
   description = "List of public IP addresses using splat syntax"
-  value       = aws_instance.web[*].public_ip
+  value       = aws_instance.ec2_instance[*].public_ip
 }
