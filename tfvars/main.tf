26eab07c031fc099d911a9d50cc6f06b2814f72e
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-07 20:34:01 +0530

    terraform

--- tfvars/main.tf
+++ tfvars/main.tf
@@ -1,61 +1,61 @@
-# resource "aws_instance" "ec2_instance" {
-#     count = 5
-#     ami = var.ami_id
-#     instance_type = var.instance_type
-#     vpc_security_group_ids = [aws_security_group.multi_env_sg.id]
-#     tags = merge(
-#         local.common_tags,
-#         {
-#             Name = "${local.common_name}-tfvars-multi-env"
-#         }
-#     )
+resource "aws_instance" "ec2_instance" {
+    count = 5
+    ami = var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.multi_env_sg.id]
+    tags = merge(
+        local.common_tags,
+        {
+            Name = "${local.common_name}-tfvars-multi-env"
+        }
+    )
 
-#     connection {
-#         user = "ec2-user"
-#         host = self.public_ip
-#         type = "ssh"
-#         password = "DevOps321"
-#     }
+    connection {
+        user = "ec2-user"
+        host = self.public_ip
+        type = "ssh"
+        password = "DevOps321"
+    }
 
-#     provisioner "remote-exec" {
-#         inline = [
-#             # "sudo yum update -y",
-#             "sudo yum install nginx -y",
-#             "sudo systemctl restart nginx",
-#             "echo Instances have been provisioned with Nginx..!"
-#         ]
-#     }
+    provisioner "remote-exec" {
+        inline = [
+            # "sudo yum update -y",
+            "sudo yum install nginx -y",
+            "sudo systemctl restart nginx",
+            "echo Instances have been provisioned with Nginx..!"
+        ]
+    }
 
-#         provisioner "remote-exec" {
-#         inline = [
-#             "sudo systemctl stop nginx",
-#             "echo Instances have been Destroyed..!"
-#         ]
-#         when = destroy
-#     }
-# }
+        provisioner "remote-exec" {
+        inline = [
+            "sudo systemctl stop nginx",
+            "echo Instances have been Destroyed..!"
+        ]
+        when = destroy
+    }
+}
 
-# resource "aws_security_group" "multi_env_sg" {
-#     name = "${local.common_name}-tfvars-multi-env"
+resource "aws_security_group" "multi_env_sg" {
+    name = "${local.common_name}-tfvars-multi-env"
 
-#     egress {
-#         from_port = 0
-#         to_port = 0
-#         protocol = "-1"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-#     ingress {
-#         from_port = 0
-#         to_port = 0
-#         protocol = "-1"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-#     tags = merge(
-#         local.common_tags,
-#         {
-#             Name = "${local.common_name}-tfvars-multi-env"
-#         }
-#     )
-# }
+    tags = merge(
+        local.common_tags,
+        {
+            Name = "${local.common_name}-tfvars-multi-env"
+        }
+    )
+}
