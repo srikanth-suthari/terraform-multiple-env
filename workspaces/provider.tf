dec68ee39276b95d4b835309bd138035dfd6928e
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-07 21:36:36 +0530

    terraform

--- workspaces/provider.tf
+++ workspaces/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
       bucket = "my-remote-state-bucket-terraform"
-      key    = "remote-state-demo"
+      key    = "workspace-demo"
       region = "us-east-1"
       use_lockfile = true
       encrypt = true
