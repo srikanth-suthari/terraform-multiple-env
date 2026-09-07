b8965dbc191101d429f0cff27937c25cf3ff0c53
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-07 06:07:24 +0530

    terraform

---
+++ dev/backend.tf
@@ -0,0 +1,5 @@
+bucket = "my-remote-state-bucket-terraform"
+key    = "remote-state-demo"
+region = "us-east-1"
+use_lockfile = true
+encrypt = true
--- provider.tf
+++ provider.tf
@@ -6,11 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-      bucket = "my-remote-state-bucket-terraform"
-      key    = "remote-state-demo"
-      region = "us-east-1"
-      use_lockfile = true
-      encrypt = true
+
   }
 }
 
