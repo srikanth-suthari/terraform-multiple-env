1ca32575e31702c03d613c399246fd5bcc16d58a
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-08 06:31:34 +0530

    terraform

--- workspaces/main.tf
+++ workspaces/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2_instance" {
     ami = var.ami_id
     instance_type = lookup(var.instance_type, terraform.workspace)
-    vpc_security_group_ids = [aws_security_group.remote-exec-demo.id]
+    vpc_security_group_ids = [aws_security_group.workspace-demo.id]
     tags = merge(
         local.common_tags,
         {
