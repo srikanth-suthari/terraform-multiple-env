25c2cae729c243f3715a13a9a1144d9a71e79678
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-08 06:31:02 +0530

    terraform

--- workspaces/locals.tf
+++ workspaces/locals.tf
@@ -1,5 +1,5 @@
 locals {
-    common_name = "${var.project}-${terraform-workspace}"
+    common_name = "${var.project}-${terraform.workspace}"
     common_tags = {
         Project = var.project
         Terraform = "true"
