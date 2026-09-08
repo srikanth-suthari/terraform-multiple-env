609ff97165a4a5f6254d7c0ff4ca784a909c9d0c
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-08 06:30:15 +0530

    terraform

--- workspaces/main.tf
+++ workspaces/main.tf
@@ -2,13 +2,16 @@ resource "aws_instance" "ec2_instance" {
     ami = var.ami_id
     instance_type = lookup(var.instance_type, terraform.workspace)
     vpc_security_group_ids = [aws_security_group.remote-exec-demo.id]
-    tags = {
-        Name = ""
-    }
+    tags = merge(
+        local.common_tags,
+        {
+            Name = local.common_name
+        }
+    )
 }
 
-resource "aws_security_group" "remote-exec-demo" {
-    name = "remote-exec-demo"
+resource "aws_security_group" "workspace-demo" {
+    name = "${var.project}-${terraform.workspace}"
 
     egress {
         from_port = 0
@@ -23,4 +26,11 @@ resource "aws_security_group" "remote-exec-demo" {
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
+
+    tags = merge(
+        local.common_tags,
+        {
+            Name = local.common_name
+        }
+    )
 }
